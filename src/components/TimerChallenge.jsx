85cef0777db07b5b6394b71e2916faf3ab8cbce0
okstas86 <stanislavocunev1@gmail.com>
2024-02-22 17:12:28 +0200

    Added timer functionality to the TimerChallenge component

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,14 +1,34 @@
+import { useState, useRef } from "react"
 export default function TimerChallenge({ title, targetTime }) {
+	const [timerExpired, setTimerExpired] = useState(false)
+	const [timerStarted, setTimerStarted] = useState(false)
+	const timer = useRef()
+
+	function handleStart() {
+		timer.current = setTimeout(() => {
+			setTimerExpired(true)
+		}, targetTime * 1000)
+		setTimerStarted(true)
+	}
+	function handleStop() {
+		clearTimeout(timer.current)
+	}
+
 	return (
 		<section className='challenge'>
 			<h2>{title}</h2>
+			{timerExpired && <p>You lost</p>}
 			<p className='challenge-time'>
 				{targetTime} second{targetTime > 1 ? "s" : ""}
 			</p>
 			<p>
-				<button>Start Challenge</button>
+				<button onClick={timerStarted ? handleStop : handleStart}>
+					{timerStarted ? "Stop" : "Start"} Challenge
+				</button>
 			</p>
-			<p></p>
+			<p className={timerStarted ? "active" : undefined}>
+				{timerStarted ? "Time is running..." : "Timer inactive"}
+			</p>
 		</section>
 	)
 }
